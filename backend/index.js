254072d33faab8e4d6de51e6b14027f9bcc1ce6c
Kritan <ramyencreeton@outlook.com>
2025-09-06 13:34:03 -0400

    basic setup done with all the folder layouts

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,23 @@
-const express = require('express');
+import express from "express";
+
+import dotenv from "dotenv";
+dotenv.config();
+
 const app = express();
-const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Define a basic route
 app.get('/', (req, res) => {
     res.send('Hello from Express!');
 });
 
+const PORT = process.env.PORT || 4000;
+
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(` Server running on http://localhost:${PORT}`);
+}).on('error', (err) => {
+    console.error(" Server failed to start:", err);
 });
