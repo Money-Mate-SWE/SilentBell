404780fc7edf0eb11d4b27d76a53cc038a1759cd
Kritan <ramyencreeton@outlook.com>
2025-09-16 15:31:35 -0400

    Added home view with tab bar and buttons

--- SilentBellApp/SilentBellApp/Views/Home/HomeView.swift
+++ SilentBellApp/SilentBellApp/Views/Home/HomeView.swift
@@ -10,10 +10,32 @@ import SwiftUI
 struct HomeView: View {
 
     var body: some View {
-        VStack {
-            Text("Welcome Home 🎉")
-            Button("Logout") {
-            }
+        TabView {
+            ContentView()
+                .tabItem {
+                    Image(systemName: "house.fill")
+                        .symbolEffect(.bounce.up.byLayer, options: .nonRepeating)
+                    Text("Home")
+                }
+            ContentView()
+                .tabItem {
+                    Image(systemName: "video.doorbell.fill")
+                        .symbolEffect(.bounce.up.byLayer, options: .nonRepeating)
+                    Text("Devices")
+                }
+            ContentView()
+                .tabItem {
+                    Image(systemName: "gearshape.fill")
+                        .symbolEffect(.bounce.up.byLayer, options: .nonRepeating)
+                    Text("Settings")
+                }
+            
         }
     }
+}
+
+
+#Preview {
+HomeView()
+    .modelContainer(for: Item.self, inMemory: true)
 }
