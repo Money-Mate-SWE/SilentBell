dbab65f3b227b7a92e720d5322d5afb64bae0f0c
Kritan <ramyencreeton@outlook.com>
2025-09-17 11:57:46 -0400

    added auth0 middleware in backend to verify user

---
+++ backend/middleware/authVerification.js
@@ -0,0 +1,9 @@
+import { auth } from "express-oauth2-jwt-bearer";
+
+const jwtCheck = auth({
+    audience: "process.env.AUTH0_AUDIENCE",
+    issuer: `https://${process.env.AUTH0_DOMAIN}/`,
+    tokenSigningAlg: 'RS256'
+});
+
+export { jwtCheck };
